404280a21b2b5faa48656d94e7fcb96cdc3713aa
Valentina Sharma <158440320+bluvitriol@users.noreply.github.com>
2024-06-08 14:37:52 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,42 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+
+    var editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.classList.add('edit-button-styling');
+
+    // toDoContainer.appendChild(paragraph);
+
+    var toDoItemContainer = document.createElement('div');
+    toDoItemContainer.classList.add('to-do-item-container');
+    toDoItemContainer.appendChild(paragraph);
+    toDoItemContainer.appendChild(editButton);
+
+    toDoContainer.appendChild(toDoItemContainer);
+
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+    editButton.addEventListener('click', function() {
+        let newText = prompt("Edit item:", paragraph.innerText);
+        if (newText !== null && newText.trim() !== "") {
+            paragraph.innerText = newText;
+        }
+    });
+})
+
+
+
+
+
